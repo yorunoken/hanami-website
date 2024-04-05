41098f58346b0be008a759ec2f80419703c46ee2
YoruNoKen <yorunoken@skiff.com>
2024-04-05 12:09:44 +0300

    delete scaling on hover in Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const HeaderContainer = styles.header``;
 
-const linksClass = "text-gray-200 text-2xl px-5 hover:scale-110 hover:text-white transition-all text-bold";
+const linksClass = "text-gray-200 text-2xl px-5 hover:text-white transition-all text-bold";
 
 export default function Header() {
     return (
