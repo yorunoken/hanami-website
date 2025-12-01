83bf5d15394e1d027445ee6293487fc22fb45452
yorunoken <me@yorunoken.com>
2025-12-01 20:44:29 +0300

    feat: add Dockerfile and .dockerignore for containerization

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    output: "standalone",
     images: {
         remotePatterns: [{ hostname: "assets.ppy.sh" }, { hostname: "yorunoken.s-ul.eu" }, { hostname: "cdn.discordapp.com" }, { hostname: "raw.githubusercontent.com" }],
     },
